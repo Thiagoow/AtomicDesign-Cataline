8f87b8119f9c50489377580232aa945e9692fd26
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2021-06-02 18:08:30 -0300

    Criado o repos

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="component"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/atoms/Logo.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="logo" src="" alt="Marcos"></div>
+</template>
+
+<style lang="scss" scoped></style>
